37daf3b797bb593477f87801e1dedadcec62fff3
agua riyanto <ar09100580@gmail.com>
2020-02-04 04:45:40 +0700

    04022020

---
+++ routes/index.js
@@ -0,0 +1,78 @@
+var express = require('express');
+var router = express.Router();
+
+var mysql = require('mysql');
+var db = mysql.createPool({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'nodejes',
+  debug: false
+})
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index');
+});
+
+router.get('/index', function (req, res, next) {
+  res.render('index');
+})
+
+//koneksi data
+router.get('/tabeldata', function(req, res, next){
+  if(db != null){
+    res.send('database terhubung');
+  }else{
+    res.send('ilang koneksinya')
+  }
+});
+
+//menampilkan data
+router.get('/data', function(req, res, next){
+  db.query('SELECT * FROM tb_item', function(err, rs){
+    res.render('data', {itemnya: rs});
+  });
+});
+
+
+//go to form input atau update data dinamis
+router.get('/inputdata', function (req, res, next) {
+    res.render('inputdata', {item: {}});
+});
+
+//fungsi tambah data
+router.post('/inputdata', function (req, res, next) {
+  db.query('INSERT INTO tb_item SET ?', req.body, function (err, rs){
+    res.redirect('/data');
+  })
+})
+
+//fungsi menghapus data
+router.get('/hapusdata', function (req, res, next) {
+  db.query('DELETE FROM tb_item WHERE id=?', req.query.id, function (err, rs) {
+    res.redirect('/data');
+  })
+});
+
+//go to form input atau update data dinamis
+router.get('/editdata', function (req, res, next) {
+  db.query('SELECT * FROM tb_item WHERE id=?', req.query.id, function (err, rs) {
+    res.render('inputdata', {item: rs[0]});
+  })
+});
+
+
+//fungsi update data
+router.post('/editdata', function (req, res, next) {
+  var param =[
+    req.body,
+    req.query.id
+  ]
+  db.query('UPDATE tb_item SET ? WHERE id=?', param, function(err, rs) {
+    res.redirect('/data');
+  })
+})
+
+
+module.exports = router;
